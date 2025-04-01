6ee8f9c8253c2cb10c861df818b59827dcf317aa
JoaoPimenta <joaov.pimenta01@gmail.com>
2025-04-01 13:21:36 -0300

    Primeiro commit do projeto

---
+++ src/main/java/io/github/JoaoPimenta/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,33 @@
+package io.github.JoaoPimenta.produtosapi.controller;
+
+import io.github.JoaoPimenta.produtosapi.model.Produto;
+import io.github.JoaoPimenta.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+}
